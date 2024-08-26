d7b15195af369a9739f3b2f0e22053a324716942
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2024-08-26 10:21:52 -0300

    Base do Projeto

---
+++ rampage/src/main/java/br/com/pi/rampage/RampageApplication.java
@@ -0,0 +1,13 @@
+package br.com.pi.rampage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RampageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RampageApplication.class, args);
+	}
+
+}
