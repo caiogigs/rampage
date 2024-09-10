faa2b688ced026ab128a1ef1854ea36c9460f346
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2024-09-10 17:27:32 -0300

    Add files via upload

---
+++ Front-end/front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ProductForm from './ProductForm';
+import ProductTable from './ProductTable';
+
+function App() {
+  return (
+    <div>
+      <ProductForm/>
+      <ProductTable/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Front-end/front/src/ProductTable.js
@@ -0,0 +1,25 @@
+function ProductTable(){
+    return(
+        <table className="table">
+            <thead >
+                <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Marca</th>
+                    <th>Selecionar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+
+    )
+}
+
+export default ProductTable;
